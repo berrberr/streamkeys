1b4c9bbbaa1d35c6ed8b153e603eaa5be09ab779
Alex <minilogo@gmail.com>
2013-09-08 01:56:17 -0400

    added next, previous, mute

--- background/background.js
+++ background/background.js
@@ -1,11 +1,11 @@
 // this script relies on bgCommunicator.js. That provides the connection outside to the content script
-
-
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "play-pause")
     communicator.notify("play-pause", { data: "foo" });
-  if (command == "play-next") 
+  if (command == "play-next")
     communicator.notify("play-next", null);
   if (command == "play-prev")
     communicator.notify("play-prev", null);
+  if (command == "mute")
+    communicator.notify("mute", null);
 });
--- contentscript/contentscript.js
+++ contentscript/contentscript.js
@@ -2,18 +2,27 @@
 
 function click(elementId) {
   $(elementId)[0].click();
-};
+}
 
-communicator.on('play-pause', function(obj) {
+communicator.on("play-pause", function(obj) {
   console.log("play-pause pressed");
   click("#play-pause");
-  console.log("click sent");
+});
+communicator.on("play-next", function(obj) {
+  console.log("play-next pressed");
+  click("#play-next");
+});
+communicator.on("play-prev", function(obj) {
+  console.log("play-prev pressed");
+  click("#play-prev");
+});
+communicator.on("mute", function(obj) {
+  console.log("mute pressed");
+  click("#volume");
 });
 
-
-communicator.on('iconClicked', function(obj) {
-  console.log('Icon was clicked. Received following data:', obj);
-
-  // confirm that the data was received (send a 'request' to the background)
-  communicator.request('confirm', { received: true });
-});
+// communicator.on('iconClicked', function(obj) {
+//   console.log('Icon was clicked. Received following data:', obj);
+//   // confirm that the data was received (send a 'request' to the background)
+//   communicator.request('confirm', { received: true });
+// });
