494f11193f683710d2ebec53ed91bb1104a3ef35
Alex Gabriel <alex@aegabriel.com>
2014-10-21 17:01:14 -0400

    Style changes to popup.

--- code/js/popup.js
+++ code/js/popup.js
@@ -9,17 +9,26 @@
     $("#options-link").setAttribute("href", chrome.runtime.getURL("html/options.html"));
 
     var toggle        = $(".toggle"),
+        button_row     = $(".top-row"),
         check_wrapper = $(".check-box-wrapper"),
         fail_message  = $("#fail-message"),
         check         = $("#check"),
         fav           = $("#tab-favicon"),
         info          = $("#info-message"),
-        red           = "#cc3737",
-        green         = "#2dcc70";
+        red           = "#CC3737",
+        green         = "#2DCC70";
+
+    var setMessage = function(disabled) {
+      if(disabled) {
+        info.innerText = "Temporarily disabled";
+      } else {
+        info.innerText = "Enabled";
+      }
+    };
 
     check.addEventListener("change", function() {
       check_wrapper.style.background = check.checked ? green : red;
-      info.innerText = "site temporarily " + (!check.checked ? "disabled" : "enabled");
+      setMessage(!check.checked);
     });
 
     chrome.tabs.getSelected(null, function(tab) {
@@ -27,18 +36,32 @@
 
       var is_disabled = chrome.extension.getBackgroundPage().window.sk_sites.check_temp_disabled(tab_url);
 
-      if (is_disabled === null || is_disabled === undefined) {
-        toggle.style.display       = "none";
-        fail_message.style.display = "block";
-
-      } else if (is_disabled === true || is_disabled === false) {
-        toggle.style.display       = "block";
+      if (is_disabled === true || is_disabled === false) {
+        toggle.style.visibility    = "visible";
+        button_row.style.display   = "block";
         check.checked              = !is_disabled;
         fail_message.style.display = "none";
-        info.innerText = "site temporarily " + (is_disabled ? "disabled" : "enabled");
+        setMessage(is_disabled);
 
         check_wrapper.style.background = check.checked ? green : red;
+        check_wrapper.style.visibility = "visible";
         fav.src = "http://g.etfv.co/" + tab_url;
+
+        if(is_disabled) {
+          chrome.browserAction.setIcon({
+            path: chrome.runtime.getURL("icon48_disabled.png"),
+            tabId: tab.id
+          });
+        } else {
+          chrome.browserAction.setIcon({
+            path: chrome.runtime.getURL("icon48.png"),
+            tabId: tab.id
+          });
+        }
+      } else {
+        toggle.style.visibility    = "hidden";
+        button_row.style.display   = "none";
+        fail_message.style.display = "inline-block";
       }
     });
   };
