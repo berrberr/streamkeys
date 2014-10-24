6958824e327dfdb7c4259cce62c0091540ca2a99
Alex Gabriel <alex@aegabriel.com>
2014-10-24 17:36:30 -0400

    Popup additions and style changes.

--- code/js/popup.js
+++ code/js/popup.js
@@ -8,28 +8,8 @@
   var onLoad = function() {
     $("#options-link").setAttribute("href", chrome.runtime.getURL("html/options.html"));
 
-    var toggle        = $(".toggle"),
-        button_row     = $(".top-row"),
-        check_wrapper = $(".check-box-wrapper"),
-        fail_message  = $("#fail-message"),
-        check         = $("#check"),
-        fav           = $("#tab-favicon"),
-        info          = $("#info-message"),
-        red           = "#CC3737",
-        green         = "#2DCC70";
-
-    var setMessage = function(disabled) {
-      if(disabled) {
-        info.innerText = "Temporarily disabled";
-      } else {
-        info.innerText = "Enabled";
-      }
-    };
-
-    check.addEventListener("change", function() {
-      check_wrapper.style.background = check.checked ? green : red;
-      setMessage(!check.checked);
-    });
+    var music_controls = $("#music-site"),
+        fail_message  = $("#fail-message");
 
     chrome.tabs.getSelected(null, function(tab) {
       tab_url = tab.url;
@@ -37,38 +17,40 @@
       var is_disabled = chrome.extension.getBackgroundPage().window.sk_sites.check_temp_disabled(tab_url);
 
       if (is_disabled === true || is_disabled === false) {
-        toggle.style.visibility    = "visible";
-        button_row.style.display   = "block";
-        check.checked              = !is_disabled;
-        fail_message.style.display = "none";
-        setMessage(is_disabled);
+        music_controls.style.display = "block";
+        fail_message.style.display   = "none";
+        // toggle.style.visibility    = "visible";
+        // button_row.style.display   = "block";
+        // check.checked              = !is_disabled;
+        // fail_message.style.display = "none";
+        // setMessage(is_disabled);
 
-        check_wrapper.style.background = check.checked ? green : red;
-        check_wrapper.style.visibility = "visible";
-        fav.src = "http://g.etfv.co/" + tab_url;
+        // check_wrapper.style.background = check.checked ? green : red;
+        // check_wrapper.style.visibility = "visible";
+        // fav.src = "http://g.etfv.co/" + tab_url;
 
-        if(is_disabled) {
-          chrome.browserAction.setIcon({
-            path: chrome.runtime.getURL("icon48_disabled.png"),
-            tabId: tab.id
-          });
-        } else {
-          chrome.browserAction.setIcon({
-            path: chrome.runtime.getURL("icon48.png"),
-            tabId: tab.id
-          });
-        }
+        // if(is_disabled) {
+        //   chrome.browserAction.setIcon({
+        //     path: chrome.runtime.getURL("icon48_disabled.png"),
+        //     tabId: tab.id
+        //   });
+        // } else {
+        //   chrome.browserAction.setIcon({
+        //     path: chrome.runtime.getURL("icon48.png"),
+        //     tabId: tab.id
+        //   });
+        // }
       } else {
-        toggle.style.visibility    = "hidden";
-        button_row.style.display   = "none";
-        fail_message.style.display = "inline-block";
+        // Not a music site
+        music_controls.style.display = "none";
+        fail_message.style.display   = "block";
       }
     });
   };
 
   document.addEventListener("DOMContentLoaded", function() {
-    $("#check").addEventListener("change", function() {
-      chrome.extension.getBackgroundPage().window.sk_sites.markAsTemporarilyDisabled(tab_url, $("#check").checked);
+    $("#enable-site").addEventListener("change", function() {
+      // chrome.extension.getBackgroundPage().window.sk_sites.markAsTemporarilyDisabled(tab_url, true);
     });
 
     onLoad();
