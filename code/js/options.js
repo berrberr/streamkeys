2ec0d6a7248e7d8a5f87b93d6e0b1e80704079b3
Alex Gabriel <alex@aegabriel.com>
2014-11-04 00:02:18 -0500

    Update styling on options page.

--- code/js/options.js
+++ code/js/options.js
@@ -3,9 +3,13 @@
 
   var $ = require("jquery");
   require("./lib/jquery.loadTemplate-1.4.4.min.js");
+  var disabledBtnClass = "btn-danger",
+      disabledSpanClass = "glyphicon-remove",
+      enabledBtnClass = "btn-success",
+      enabledSpanClass = "glyphicon-ok";
 
-  function render_site_checkbox(id, name, checked) {
-    var template_name = checked ? "#template-enabled" : "#template-disabled";
+  var renderSiteButton = function(id, name, checked) {
+    var template_name = (checked === "true") ? "#template-enabled" : "#template-disabled";
     $("#sitelist").loadTemplate(
       $(template_name),
       {
@@ -15,17 +19,39 @@
       },
       {append: true}
     );
-  }
+  };
+
+  var toggleSiteButton = function(selector, is_disabled) {
+    var el = $(selector),
+        span = $($(selector).children("span")[0]);
+    var btnClass = (is_disabled === "false") ? [disabledBtnClass, enabledBtnClass] : [enabledBtnClass, disabledBtnClass];
+    var spanClass = (is_disabled === "false") ? [disabledSpanClass, enabledSpanClass] : [enabledSpanClass, disabledSpanClass];
+    el.addClass(btnClass[0]).removeClass(btnClass[1]);
+    span.addClass(spanClass[0]).removeClass(spanClass[1]);
+  };
 
   chrome.runtime.sendMessage({action: "get_sites"}, function(response) {
     console.log("RESP: ", response);
     $.each(response, function(key, val) {
-      render_site_checkbox(key, val.name, val.enabled);
+      renderSiteButton(key, val.name, val.enabled);
     });
 
     //On clicking a site name checkbox
-    $(".btn-site-enable").click(function() {
-      var sites = {};
+    $(".btn-site-enable").click(function(el) {
+      var sites = {},
+          el_id = "[id=\"" + el.currentTarget.id + "\"]";
+
+      console.log(el.currentTarget.id);
+      console.log(el_id);
+
+      // These are strings not bools, so inverse the attr value with str compare
+      var toggleVal = $(el_id).attr("is-enabled") === "true" ? "false" : "true";
+      $(el_id).attr("is-enabled", toggleVal);
+
+      // Toggle the button state
+      toggleSiteButton(el_id, $(el_id).attr("is-enabled"));
+
+      // Generate localstorage object of form {sitename => enabled}
       $(".btn-site-enable").each(function(index, site) {
         sites[$(site).attr("id")] = $(site).attr("is-enabled");
       });
