1d9ad1bf72267bcfacddc73326e55db56840d722
Alex Gabriel <alex@aegabriel.com>
2014-11-28 17:10:35 -0500

    Only change icon if it's a music site.

--- code/js/contentscript.js
+++ code/js/contentscript.js
@@ -3,10 +3,10 @@
 
   chrome.runtime.sendMessage({action: "check_music_site"}, function(resp) {
     if(resp !== "no_inject") {
-      // Set the browser action icon based on site enabled/disabled status
-      chrome.runtime.sendMessage({action: "set_icon"});
-
       if(resp) {
+        // Set the browser action icon based on site enabled/disabled status
+        chrome.runtime.sendMessage({action: "set_icon"});
+
         chrome.runtime.sendMessage({action: "get_site_controller"}, function(controller) {
           if(controller) chrome.runtime.sendMessage({action: "inject_controller", file: "js/controllers/" + controller});
         });
