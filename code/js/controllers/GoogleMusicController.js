ed8d3ff7fa45df06bcad68a1bd276b7fa8c032cb
Yury <y.timofeyev@gmail.com>
2014-12-09 22:19:42 +0300

    like & dislike hotkeys for Google Music

--- code/js/controllers/GoogleMusicController.js
+++ code/js/controllers/GoogleMusicController.js
@@ -4,6 +4,8 @@
   require("BaseController").init({
     playPause: "[data-id=play-pause]",
     playNext: "[data-id=forward]",
-    playPrev: "[data-id=rewind]"
+    playPrev: "[data-id=rewind]",
+    like: "[data-rating=5]",
+    dislike: "[data-rating=1]"
   });
 })();
