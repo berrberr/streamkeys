74f3a20308ee8be32f13259ac837a786702a8eae
Alex Gabriel <alex@aegabriel.com>
2014-12-22 12:02:13 -0500

    Update travis secrets file.

--- travis-prcheck.sh
+++ travis-prcheck.sh
@@ -2,5 +2,5 @@
 set -ev
 echo $TRAVIS_PULL_REQUEST
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  openssl aes-256-cbc -K $encrypted_b30e2c7e7c3f_key -iv $encrypted_b30e2c7e7c3f_iv -in secrets.json.enc -out test/secrets.json -d
+  openssl aes-256-cbc -K $encrypted_e72e740b7464_key -iv $encrypted_e72e740b7464_iv -in secrets.json.enc -out test/secrets.json -d
 fi
