477d9289b8068fc5b4239848b83b895e45514231
Brandon Yuhas <brandonjyuhas@gmail.com>
2015-04-05 22:51:22 -0400

    Add TheDrop.Club

---
+++ code/js/controllers/ThedropController.js
@@ -0,0 +1,12 @@
+;(function() {
+  "use strict";
+
+  require("BaseController").init({
+    siteName: "The Drop",
+    play: ".play-button",
+    pause: ".pause-button",
+    playNext: ".next-button",
+    playPrev: ".prev-button",
+    mute: ".volume-button"
+  });
+})();
