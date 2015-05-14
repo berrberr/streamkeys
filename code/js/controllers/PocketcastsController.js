f1ed87e9a3c232dcb734ffeab05534c6556c8491
Alex Gabriel <alex@aegabriel.com>
2015-05-13 23:41:26 -0400

    Fix pocketcasts. Fix youtube for new player.

--- code/js/controllers/PocketcastsController.js
+++ code/js/controllers/PocketcastsController.js
@@ -6,6 +6,10 @@
     playPause: ".play_pause_button",
     playNext: ".skip_forward_button",
     playPrev: ".skip_back_button",
-    mute: ".audio_volume_slider"
+    mute: ".audio_volume_slider",
+
+    playState: ".play_pause_button.pause_button",
+    song: ".player_episode",
+    artist: ".player_podcast_title"
   });
 })();
--- code/js/controllers/YoutubeController.js
+++ code/js/controllers/YoutubeController.js
@@ -29,4 +29,25 @@
     if(document.querySelector(this.selectors.playPrev) === null) sk_log("disabled. Playlist selectors not found!");
     else this.click({selectorButton: this.selectors.playPrev, action: "playPrev"});
   };
+
+  controller.click = function(opts) {
+    opts = opts || {};
+    if(opts.selectorButton === null) {
+      sk_log("disabled", opts.action);
+      return;
+    }
+
+    var doc = (opts.selectorFrame) ? document.querySelector(opts.selectorFrame).contentWindow.document : document;
+    if (!doc) return;
+
+    try {
+      doc.querySelector(opts.selectorButton).click();
+      sk_log(opts.action);
+    } catch(e) {
+      // If selector fails then we are using the new youtube player
+      if(opts.action === "playPause") doc.querySelector(".ytp-play-button").click();
+      if(opts.action === "playNext") doc.querySelector(".ytp-next-button").click();
+      if(opts.action === "playPrev") doc.querySelector(".ytp-prev-button").click();
+    }
+  };
 })();
