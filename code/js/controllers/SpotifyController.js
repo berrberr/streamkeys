3cf5a529ef1067f65666fd9b7ef9a11ddba7f5b4
Alex Gabriel <alex@aegabriel.com>
2015-06-14 10:43:10 -0700

    Add multi-player support for new Spotfy UI.

--- code/js/controllers/SpotifyController.js
+++ code/js/controllers/SpotifyController.js
@@ -1,7 +1,20 @@
 ;(function() {
   "use strict";
 
-  require("BaseController").init({
+  var controller = require("BaseController"),
+      $ = require("jquery");
+
+  var multiSelectors = {
+    playPause: ["#play-pause", "#play"],
+    playNext: ["#next", "#next"],
+    playPrev: ["#previous", "#previous"],
+    playState: ["#play-pause.playing", "#play.playing"],
+    iframe: ["#app-player", "#main"],
+    song: ["#track-name", ".caption .track"],
+    artist: ["#track-artist", ".caption .artist"]
+  };
+
+  controller.init({
     siteName: "Spotify",
     playPause: "#play-pause",
     playNext: "#next",
@@ -13,4 +26,21 @@
     song: "#track-name",
     artist: "#track-artist"
   });
+
+
+  controller.checkPlayer = function() {
+    var that = this,
+        doc = document.querySelector(multiSelectors.iframe[0]);
+
+    if(doc.querySelector(multiSelectors.playPause[0])) {
+      $.each(multiSelectors, function(key, value) {
+        that.selectors[key] = value[0];
+      });
+    }
+    else {
+      $.each(multiSelectors, function(key, value) {
+        that.selectors[key] = value[1];
+      });
+    }
+  };
 })();
