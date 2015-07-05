ede0c2a1ca378b123a562f82af56e69d91ba66b3
Eric Nemchik <eric@nemchik.com>
2015-07-05 10:43:16 -0500

    Make canPlayPause a bit more human readable

--- code/js/modules/BaseController.js
+++ code/js/modules/BaseController.js
@@ -212,8 +212,11 @@
       siteName: this.siteName,
       canDislike: !!(this.selectors.dislike && this.doc().querySelector(this.selectors.dislike)),
       canPlayPrev: !!(this.selectors.playPrev && this.doc().querySelector(this.selectors.playPrev)),
-      canPlayPause: !!((this.selectors.playPause && this.doc().querySelector(this.selectors.playPause)) ||
-        (this.selectors.play && this.doc().querySelector(this.selectors.play)) || (this.selectors.pause && this.doc().querySelector(this.selectors.pause))),
+      canPlayPause: !!(
+        (this.selectors.playPause && this.doc().querySelector(this.selectors.playPause)) ||
+        (this.selectors.play && this.doc().querySelector(this.selectors.play)) ||
+        (this.selectors.pause && this.doc().querySelector(this.selectors.pause))
+      ),
       canPlayNext: !!(this.selectors.playNext && this.doc().querySelector(this.selectors.playNext)),
       canLike: !!(this.selectors.like && this.doc().querySelector(this.selectors.like))
     };
