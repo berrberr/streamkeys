e17a195c3d22d1a1f0facb9a67cf670bfb4ccdfc
Jamie Lentin <jm@lentin.co.uk>
2015-08-29 14:51:29 +0100

    Use BBC radio player accessibility links to go forwards/back in time

--- code/js/controllers/BBCRadioController.js
+++ code/js/controllers/BBCRadioController.js
@@ -6,6 +6,8 @@
     siteName: "BBC Radio",
     play: "#play",
     pause: "#pause",
+    playNext: ".od-skip[data-offset='60']",
+    playPrev: ".od-skip[data-offset='-60']",
     mute: "#volume-mute",
     like: "#toggle-mystations:not(.in-mystations) > span",
     dislike: "#toggle-mystations.in-mystations > span",
