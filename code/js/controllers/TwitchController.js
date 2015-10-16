95a75b1dcbabb9f1150b7e0d4f0e2758220060dc
Alex Gabriel <alexander@sensortower.com>
2015-10-16 00:53:01 -0400

    Fix twitch.tv controller.

--- code/js/controllers/TwitchController.js
+++ code/js/controllers/TwitchController.js
@@ -1,48 +1,16 @@
 ;(function() {
   "use strict";
 
-  var controller = require("BaseController"),
-      sk_log = require("../modules/SKLog.js");
+  var controller = require("BaseController");
 
   controller.init({
     siteName: "Twitch.tv",
-    playPause: "#override",
+    playPause: ".player-button--playpause",
     song: ".title .real"
   });
 
-  controller.overridePlayPause = true; // Override here so controls are enabled in popup
-  controller.selectors.player = "object[data$=TwitchPlayer\\.swf]";
-  controller.player = null;
-
   controller.isPlaying = function() {
-    if(this.player && this.player.isPaused) {
-      return !this.player.isPaused();
-    } else {
-      try {
-        this.player = document.querySelector(this.selectors.player);
-      } catch (e) {
-        sk_log("Twitch player not found", e, true);
-        return false;
-      }
-    }
-  };
-
-  controller.playPause = function() {
-    if(this.isPlaying()) {
-      try {
-        this.player.pauseVideo();
-        sk_log("playPause");
-      } catch(e) {
-        sk_log("Twitch player error", e, true);
-      }
-    } else {
-      try {
-        this.player.playVideo();
-        sk_log("playPause");
-      } catch(e) {
-        sk_log("Twitch player error", e, true);
-      }
-    }
+    return !!(this.doc().querySelector("span[data-tip='Pause']"));
   };
 
 })();
