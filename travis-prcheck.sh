0e1db744ec8702d894bdaf16a5db1f8966649c3a
Alex Gabriel <alex@aegabriel.com>
2015-11-23 00:58:13 -0800

    Cleanup old tests.

--- travis-prcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-echo $TRAVIS_PULL_REQUEST
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  openssl aes-256-cbc -K $encrypted_e72e740b7464_key -iv $encrypted_e72e740b7464_iv -in secrets.json.enc -out test/secrets.json -d
-fi
