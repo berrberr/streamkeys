156e63f4c0e3a8a2563afb288f691b5a8ce9dccb
Nati Nazarov <nati@linux.com>
2016-04-06 17:57:52 +0300

    Disi.co.il support

---
+++ code/js/controllers/DisiController.js
@@ -0,0 +1,14 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Disi",
+    playPause: ".youtube-player",
+    song: ".song_container .title h3",
+    playNext: ".youtube-player-toolbar li:nth-child(2)",
+    playPrev: ".youtube-player-toolbar li"
+  });
+
+})();
