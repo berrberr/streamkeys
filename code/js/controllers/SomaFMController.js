0f09dc9691ca8accce21a986213c50d2128c95a8
Malachi Soord <inverse.chi@gmail.com>
2016-06-15 12:15:32 +0200

    Add support for soma.fm

---
+++ code/js/controllers/SomaFMController.js
@@ -0,0 +1,11 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "SomaFM",
+    play: "#playBtn",
+    pause: "#stopBtn"
+  });
+})();
