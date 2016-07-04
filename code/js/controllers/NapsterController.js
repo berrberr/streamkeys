62172d1534ab5633726dfb51ff033e688be56d70
Frederik Vinnen <frederik.vinnen@apploft.de>
2016-07-04 14:42:52 +0200

    Added NapsterController

---
+++ code/js/controllers/NapsterController.js
@@ -0,0 +1,19 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Napster",
+	play: ".player-play-button .icon-play-button",
+    pause: ".player-play-button .icon-pause2",
+    playNext: ".player-advance-button",
+    playPrev: ".player-rewind-button",
+	like: ".like-button",
+	dislike: ".dislike-button",
+	buttonSwitch: true,
+	
+    song: ".player-track",
+    artist: ".player-artist"
+  });
+})();
