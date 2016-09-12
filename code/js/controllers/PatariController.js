f52a2c3d13325c6a0ce53691fcb2656cf9d6d5da
Faheem Memon <memon.faheem@gmail.com>
2016-09-12 19:33:50 -0400

    Added support for http://patari.pk

---
+++ code/js/controllers/PatariController.js
@@ -0,0 +1,15 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Patari",
+    playPause: ".player .playerPlay",
+    playNext: ".player .playerMove[title='Next Song']",
+    playPrev: ".player .playerMove[title='Previous Song']",
+
+    song: ".player .songName",
+    artist: ".player .artistName"
+  });
+})();
