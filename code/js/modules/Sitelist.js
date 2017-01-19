dae8e5ee11c409f8e89bf28f2131fa9b2a439212
Dale Sedivec <dale@codefu.org>
2017-01-19 13:13:53 -0600

    Add support for radd.it

--- code/js/modules/Sitelist.js
+++ code/js/modules/Sitelist.js
@@ -121,6 +121,7 @@
       "plex": { name: "Plex", url: "http://www.plex.tv" },
       "pluralsight": { name: "Pluralsight", url: "https://app.pluralsight.com" },
       "pocketcasts": { name: "Pocketcasts", url: "https://play.pocketcasts.com" },
+      "radd": { name: "radd.it", url: "http://radd.it", controller: "RadditController.js" },
       "radioparadise": { name: "RadioParadise", url: "http://www.radioparadise.com" },
       "radioswissjazz": { name: "RadioSwissJazz", url: "http://www.radioswissjazz.ch" },
       "radiotunes": { name: "RadioTunes", url: "http://www.radiotunes.com" },
