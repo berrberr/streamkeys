6e29cb6d661284724fea6c8c8b839711913e45eb
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-02-10 23:38:46 +0900

    add a new controller of driveplayer.com

---
+++ code/js/controllers/DriveplayerController.js
@@ -0,0 +1,20 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Drive Player",
+
+    play: ".jp-controls .jp-play",
+    pause: ".jp-controls .jp-pause",
+    playNext: ".jp-controls .jp-next",
+    playPrev: ".jp-controls .jp-previous",
+    mute: ".jp-controls .jp-mute",
+
+    playState: ".jp-controls .jp-pause[style=\"display: block;\"]",
+    artist: ".song.playing .artist",
+    song: ".song.playing .title",
+
+  });
+})();
