4145d288f0f868493a37fd5657e644e8ffe7ab39
Benjamin Sinkula <bsinky@gmail.com>
2017-03-08 07:36:50 -0600

    Fix Spotify controller (fixes #283)

--- code/js/controllers/SpotifyController.js
+++ code/js/controllers/SpotifyController.js
@@ -1,50 +1,16 @@
 ;(function() {
   "use strict";
 
-  var BaseController = require("BaseController"),
-      _ = require("lodash");
+  var BaseController = require("BaseController");
 
-  var multiSelectors = {
-    play: [null, null, ".buttons [title='Play']"],
-    pause: [null, null, ".buttons [title='Pause']"],
-    playPause: ["#play-pause", "#play", null],
-    playNext: ["#next", "#next", ".buttons [title='Next']"],
-    playPrev: ["#previous", "#previous", ".buttons [title='Previous']"],
-    playState: ["#play-pause.playing", "#play.playing", ""],
-    iframe: ["#app-player", "#main", ".nowPlayingBar-container"],
-    buttonSwitch: ["false", "false", "true"],
-    like: [".thumb.up", ".thumb.up", ""],
-    dislike: [".thumb.down", ".thumb.down" ,""],
-    song: ["#track-name", ".caption .track", ".now-playing-bar :first-child :nth-child(2) :nth-child(1) :first-child"],
-    artist: ["#track-artist", ".caption .artist", ".now-playing-bar :first-child :nth-child(2) :nth-child(2) :first-child"]
-  };
+  new BaseController({
+    siteName: "Spotify",
+    playPause: "[title='Pause'],[title='Play']",
+    playNext: "[title='Next']",
+    playPrev: "[title='Previous']",
 
-  var controller = new BaseController({
-    siteName: "Spotify"
+    playState: "[title='Pause']",
+    song: ".nowPlayingBar-container [href*='/album/']",
+    artist: ".nowPlayingBar-container [href*='/artist/']"
   });
-
-  controller.checkPlayer = function() {
-    var that = this;
-    var player = document.querySelector(multiSelectors.iframe[0]) ? "v1" : "v2";
-    player = document.querySelector(multiSelectors.iframe[2]) ? "v3" : player;
-
-    if(player == "v1") {
-      _.each(multiSelectors, function(value, key) {
-        that.selectors[key] = value[0];
-      });
-      that.buttonSwitch = false;
-    }
-    else if(player == "v2"){
-      _.each(multiSelectors, function(value, key) {
-        that.selectors[key] = value[1];
-      });
-      that.buttonSwitch = false;
-    }
-    else{
-      _.each(multiSelectors, function(value, key) {
-        that.selectors[key] = value[2];
-      });
-      that.buttonSwitch = true;
-    }
-  };
 })();
