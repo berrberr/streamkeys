5c16f31bcb27d434ce56eb2326fa8d7b1ebf8de4
leocaseiro <leobok@gmail.com>
2017-03-25 17:42:09 +1100

    feature(Udemy): add support for Udemy

---
+++ code/js/controllers/UdemyController.js
@@ -0,0 +1,16 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Udemy",
+    playPause: ".vjs-play-control",
+    play: ".vjs-play-control",
+    pause: ".vjs-play-control",
+    playNext: ".btn-control[class*=continue-button]",
+    mute: ".vjs-audio-button",
+    playState: ".vjs-playing",
+    buttonSwitch: true
+  });
+})();
