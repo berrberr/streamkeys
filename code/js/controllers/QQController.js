51a8afc7ca6b3e6d5fe3d959398a34297fb0a32b
Ric Ren <ric.ren@turn.com>
2017-04-20 15:33:09 -0700

    * Create QQController.js * Add y.qq.com/portal/player.html

---
+++ code/js/controllers/QQController.js
@@ -0,0 +1,18 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "QQ Music",
+    playPause: "#btnplay",
+    playNext: ".btn_big_next",
+    playPrev: ".btn_big_prev",
+    like: ".btn_big_like",
+
+    playState: "#btnplay.btn_big_play--pause",
+    song: "#song_name a",
+    artist: "#singer_name a",
+    album: "#album_name a"
+  });
+})();
