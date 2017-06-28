39b9dbb431dba8c7dc5952c3a7dcd6f7a516ce3c
Maksim Ovcharik <maksim.ovcharik@gmail.com>
2017-06-28 03:07:53 +0300

    Fix mute for YandexRadio controller

--- code/js/controllers/YandexRadioController.js
+++ code/js/controllers/YandexRadioController.js
@@ -1,18 +1,24 @@
 ;(function() {
   "use strict";
 
-  var BaseController = require("BaseController");
-
-  new BaseController({
+  var MouseEventController = require("MouseEventController");
+  var yandexRadioController = new MouseEventController({
     siteName: "Yandex Radio",
+
     playPause: ".player-controls__play",
     playNext: ".slider__item_next",
+    playState: ".body_state_playing",
+
     mute: ".volume__btn",
     like: ".player-controls .like_action_like",
     dislike: ".player-controls .like_action_dislike",
 
-    playState: ".body_state_playing",
     song: ".player-controls__title",
     artist: ".player-controls__artists"
   });
+
+  // override mute
+  yandexRadioController.mute = function() {
+    this.mousedown({action: "mute", selectorButton: this.selectors.mute, selectorFrame: this.selectors.iframe});
+  };
 })();
