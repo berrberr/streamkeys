0d4171a1295e358108588341fd7a3dc3eabae56a
Shaya Goldberg <shaya.goldberg@aig.com>
2017-07-26 09:18:44 -0400

    add support for iBroadcast

---
+++ code/js/controllers/IbroadcastController.js
@@ -0,0 +1,18 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "iBroadcast",
+    play: ".mgr-player-controls .mgr-player-play",
+    pause: ".mgr-player-controls .mgr-player-play",
+    playPrev: ".mgr-player-controls .mgr-player-prev",
+    playNext: ".mgr-player-controls .mgr-player-next",
+
+    buttonSwitch: ".mgr-player-controls .mgr-player-play[src='/svg/btn-play.svg']",
+    song: ".mgr-player-trackinfo .mgr-player-title",
+    artist: ".mgr-player-trackinfo .mgr-player-artist",
+    album: ".mgr-player-trackinfo .mgr-player-album"
+  });
+})();
