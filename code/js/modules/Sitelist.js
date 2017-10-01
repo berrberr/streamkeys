366d5e50f914d5a66d59e0ffaa79234fbdaa244b
papers <paperslove@users.noreply.github.com>
2017-10-01 21:22:00 +0300

    Added StreamElements support (#347)
    added streamelements

--- code/js/modules/Sitelist.js
+++ code/js/modules/Sitelist.js
@@ -146,6 +146,7 @@
       "spotify": { name: "Spotify Web Player", url: "http://www.spotify.com" },
       "spreaker": { name: "Spreaker", url: "http://www.spreaker.com" },
       "stitcher": { name: "Stitcher", url: "http://www.stitcher.com" },
+      "streamelements": { name: "StreamElements", url: "https://streamelements.com/dashboard/songrequest/general" },
       "streamsquid": { name: "StreamSquid", url: "http://streamsquid.com/" },
       "subsonic": { name: "Subsonic", url: "http://www.subsonic.org" },
       "tidal": { name: "Tidal", url: "https://www.tidal.com", alias: ["tidalhifi"] },
