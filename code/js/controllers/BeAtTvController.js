c14c8edfe3cb3affc84e532788a34fcf675e214a
Tommy Quissens <tommy.quissens@storefront.be>
2017-10-19 13:34:48 +0200

    ADD: be-at.tv support

---
+++ code/js/controllers/BeAtTvController.js
@@ -0,0 +1,15 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "BE-AT.TV",
+    play: ".btn.playbutton",
+    pause: ".btn.pausebutton",
+    mute: ".btn.volume",
+    song: ".ticker",
+    playState: ".btn.pausebutton"
+    // buttonSwitch: true
+  });
+})();
