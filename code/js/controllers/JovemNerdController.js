9aa50c49f0b41731ad708b258029aad45b52fe12
Lucas de Castro <lucascastro102@gmail.com>
2017-11-17 13:18:28 -0300

    Add JovemNerdController

---
+++ code/js/controllers/JovemNerdController.js
@@ -0,0 +1,16 @@
+(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Jovem Nerd",
+    play: ".player__primary-controllers__play-pause .icon-play",
+    pause: ".player__primary-controllers__play-pause .icon-pause",
+    playNext: ".player__primary-controllers__15s-forward",
+    playPrev: ".player__primary-controllers__15s-back",
+    song: ".entry-title",
+    buttonSwitch: true,
+    hidePlayer: true
+  });
+})();
