9824939848eb73bb5d3b5d3b5066e2f5c45af86e
Mike Cluck <mcluck90@gmail.com>
2018-01-31 11:56:36 -0700

    Added the buttonSwitch flag because it was having trouble without it

--- code/js/controllers/BrainFMController.js
+++ code/js/controllers/BrainFMController.js
@@ -5,6 +5,7 @@
 
   new BaseController({
     siteName: "BrainFM",
-    playPause: ".modules-music-player-css-PlayControl__wrapper___2ROhW"
+    playPause: ".modules-music-player-css-PlayControl__wrapper___2ROhW",
+    buttonSwitch: true
   });
 })();
