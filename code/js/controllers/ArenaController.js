6dab2ca8871ec2a741a9946e9f548c231d4fd69c
Michael Reynolds <michael.reynolds@paybase.io>
2018-03-14 19:18:10 +0000

    add arena controller

---
+++ code/js/controllers/ArenaController.js
@@ -0,0 +1,16 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Are.na",
+    play: "#playPause button:nth-child(2)",
+    pause: "#playPause button:nth-child(2)",
+    playNext: "#playPause button:nth-child(3)",
+    playPrev: "#playPause button:nth-child(1)",
+    buttonSwitch: true,
+    playState: "#nowPlaying .playerPlaying",
+    song: "#nowPlaying .tile-wrap-full",
+  });
+})();
