b6be2233924fd942cdd746b35637f25003555789
long-m-r <long-m-r@users.noreply.github.com>
2018-03-19 20:22:13 -0500

    Updated AudibleController.js
    Audible's cloud player has changed at some point. This update fixes it. Fixes Issue #379

--- code/js/controllers/AudibleController.js
+++ code/js/controllers/AudibleController.js
@@ -3,15 +3,38 @@
 
   var BaseController = require("BaseController");
 
-  new BaseController({
+  var controller = new BaseController({
     siteName: "Audible",
-    play: ".main_ctrl .play",
-    pause: ".main_ctrl .pause",
-    playPrev: ".main_ctrl .repeat",
 
-    playState: ".main_ctrl .play.hide",
-    song: ".chapter",
+    play: "#adbl-cloud-player-controls .adblPlayButton",
+    pause: "#adbl-cloud-player-controls .adblPauseButton",
+    playPrev: "#adbl-cloud-player-controls .adblFastRewind",
+    playNext: "#adbl-cloud-player-controls .adblFastForward",
+    buttonSwitch: false,
+    song: "#cp-Top-chapter-display",
+    art: "#adbl-cloudBook",
 
-    hidePlayer: true
+    playState: ".adblPlayButton.bc-hidden"
   });
+
+  controller.isPlaying = function() {
+    var playEl = this.doc().querySelector(this.selectors.play),
+        isPlaying = false;
+
+    if(this.buttonSwitch) {
+      // If playEl does not exist then it is currently playing
+      isPlaying = (playEl === null);
+    }
+    else if(this.selectors.playState) {
+      // Check if the play state element exists and is visible
+      var playStateEl = this.doc().querySelector(this.selectors.playState);
+      // Override the second check here since it fails even though it shouldn't
+      isPlaying = !!(playStateEl);
+    }
+    else if(playEl) {
+      isPlaying = (window.getComputedStyle(playEl, null).getPropertyValue("display") === "none");
+    }
+    return isPlaying;
+  };
+
 })();
