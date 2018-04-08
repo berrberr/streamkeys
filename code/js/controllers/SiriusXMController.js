5995cb55c5600dc884da3335ffb7c7a0dacc3593
Camron <camronquadri@gmail.com>
2018-04-08 14:48:25 -0400

    SiriusXM Support (#415)
    Support for Sirius XM Satellite Radio

---
+++ code/js/controllers/SiriusXMController.js
@@ -0,0 +1,18 @@
+;(function() {
+    "use strict";
+
+    var BaseController = require("BaseController");
+
+    new BaseController({
+      siteName: "SiriusXM",
+      playPause: ".play",
+      playNext: ".next",
+      playPrev: ".prev",
+      mute: ".minimized-volume-control",
+
+      playState: ".play[aria-label='Pause audio']",
+      song: ".np-track-artist",
+      album: ".np-track-show-name", //"Show" name
+      art: "#fallbackImg"
+    });
+  })();
