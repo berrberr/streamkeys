b5ff244b0638cddf52b6bd6ed01c1e602866dcb0
Alex Tsitsimpis <alex.tsitsimpis@gmail.com>
2018-09-16 08:12:43 +0300

    bandcamp: Add support to get cover art
    Signed-off-by: Alex Tsitsimpis <alex.tsitsimpis@gmail.com>

--- code/js/controllers/BandcampController.js
+++ code/js/controllers/BandcampController.js
@@ -3,7 +3,7 @@
 
   var BaseController = require("BaseController");
 
-  new BaseController({
+  var controller = new BaseController({
     siteName: "Bandcamp",
     playPause: ".playbutton",
     playNext: ".nextbutton",
@@ -12,10 +12,21 @@
     playState: ".playbutton.playing",
     song: "a.title_link > span.title",
     artist: "[itemprop=byArtist]",
+    art: ".popupImage",
 
     hidePlayer: true,
 
     currentTime: ".time_elapsed",
     totalTime: ".time_total"
   });
+
+
+  controller.getArtData = function(selector) {
+    var dataEl = this.doc().querySelector(selector);
+    if(dataEl && dataEl.attributes && dataEl.attributes.href) {
+      return dataEl.attributes.href.value;
+    }
+
+    return null;
+  };
 })();
