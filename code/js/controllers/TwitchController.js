37f7f3c748fbcfec19385ccfbec5150efa4a8c47
cubimon <cubimon93@gmail.com>
2018-11-11 19:36:01 +0100

    add volume control to twitch

--- code/js/controllers/TwitchController.js
+++ code/js/controllers/TwitchController.js
@@ -13,4 +13,19 @@
     return !!(this.doc().querySelector("span[data-tip='Pause']"));
   };
 
+  controller.getVolume = function() {
+    var videos = document.getElementsByTagName("video");
+    if(videos.length > 0) {
+      return videos[0].volume;
+    }
+    return 1;
+};
+
+  controller.setVolume = function(volume) {
+    var videos = document.getElementsByTagName("video");
+    if(videos.length > 0) {
+      videos[0].volume = volume;
+    }
+};
+
 })();
