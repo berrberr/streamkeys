7f2a2a0abe962a2f04847b5127c063b83ca4285e
Dennis Skinner <skinner927@gmail.com>
2018-12-10 15:53:04 -0500

    Allow BaseController selectors to be getter functions.
    - Also add "next mix" to 8Tracks' "playNext" handler

--- code/js/controllers/8tracksController.js
+++ code/js/controllers/8tracksController.js
@@ -2,12 +2,22 @@
   "use strict";
 
   var BaseController = require("BaseController");
+  var playNextOptions = ["#player_skip_button", "#next_mix_button"];
 
   new BaseController({
     siteName: "8tracks",
     play: "#player_play_button",
     pause: "#player_pause_button",
-    playNext: "#player_skip_button",
+    playNext: function() {
+      for (var i = 0; i < playNextOptions.length; i++) {
+        var el = this.doc().querySelector(playNextOptions[i]);
+        if (el && el.style.display !== "none") {
+          return playNextOptions[i];
+        }
+      }
+      // None are valid, just return the first selector
+      return playNextOptions[0];
+    },
     mute: ".volume-mute",
     like: ".mix-like.inactive",
     dislike: ".mix-like.active",
