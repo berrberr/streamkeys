b04a2304ada9039f1be70029e22a087081f0e86c
Tim Dorr <timdorr@users.noreply.github.com>
2019-01-05 22:13:08 -0500

    Fix Hulu support
    They changed some CSS classes, so this updates things for the new ones.

--- code/js/controllers/HuluController.js
+++ code/js/controllers/HuluController.js
@@ -5,17 +5,17 @@
 
   var controller = new MouseEventController({
     siteName: "Hulu",
-    play: "div.play-pause-button",
-    pause: "div.play-pause-button",
-    playNext: "div.next-video-button",
-    playPrev: "div.rewind-button",
-    mute: "div.volume-image",
+    play: "div.controls__playback-button-playing",
+    pause: "div.controls__playback-button-paused",
+    playNext: "div.controls__fastforward-button",
+    playPrev: "div.controls__rewind-button",
+    mute: "div.controls__volume-button-mute'e",
 
-    song: ".pause-title",
-    buttonSwitch: true
+    song: ".metadata-area__third-line",
+    buttonSwitch: false
   });
 
   controller.isPlaying = function () {
-    return !!(this.doc().querySelector("div.play-pause-button.playing"));
+    return !!(this.doc().querySelector("div.controls__playback-button--playing"));
   };
 })();
