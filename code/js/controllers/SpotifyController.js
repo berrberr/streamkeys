6695fcb0902773c30f773adb0f495b4a6794773b
Eric Nemchik <eric@nemchik.com>
2019-05-22 10:13:36 -0500

    Update Spotify controller

--- code/js/controllers/SpotifyController.js
+++ code/js/controllers/SpotifyController.js
@@ -9,13 +9,13 @@
     pause: ".now-playing-bar button[class*=pause]",
     playNext: ".now-playing-bar button[class*=skip-forward]",
     playPrev: ".now-playing-bar button[class*=skip-back]",
-    like: ".now-playing button",
-    dislike: ".now-playing-bar button[class*=thumbs-down]",
+    like: ".now-playing-bar button[class*=heart]",
+    dislike: ".now-playing-bar button[class*=block]",
     buttonSwitch: true,
     mute: ".now-playing-bar button[class*=volume]",
     song: ".now-playing-bar .track-info__name",
     artist: ".now-playing-bar .track-info__artists",
-    art: "div.now-playing-bar__left div.cover-art-image.cover-art-image-loaded",
+    art: ".now-playing-bar .cover-art-image-loaded",
 
     // Messy nth-child selectors, but there is no other class/id/attribute information to distinguish the two times
     currentTime: ".now-playing-bar .playback-bar__progress-time:nth-child(1)",
@@ -24,12 +24,11 @@
 
   // Spotify art uses an inline CSS background-image style, this override parses the image from there
   controller.getArtData = function(selector) {
-    if(!selector) return null;
+    if (!selector) return null;
 
     var dataEl = this.doc().querySelector(selector);
 
-    if (dataEl !== null)
-    {
+    if (dataEl !== null) {
       var backgroundImage = window.getComputedStyle(dataEl)["background-image"];
       return backgroundImage.match(/url\(["|']?([^"']*)["|']?\)/)[1];
     }
