9a1c59538199887adca535621bf5942e938b55e9
Dani <danidosixto@gmail.com>
2019-07-30 11:15:21 -0400

    Update BrainFMController.js
    Button controls class name changed, updated accordingly.

--- code/js/controllers/BrainFMController.js
+++ code/js/controllers/BrainFMController.js
@@ -5,7 +5,7 @@
 
   new BaseController({
     siteName: "BrainFM",
-    playPause: ".modules-music-player-css-PlayControl__wrapper___2ROhW",
+    playPause: ".PlayControl__wrapper___341vD",
     buttonSwitch: true
   });
 })();
