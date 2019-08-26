50c8eea85a17895163c18ae5b33022bc4d32b351
Eric Nemchik <eric@nemchik.com>
2019-08-26 14:34:11 -0500

    Fix linting errors

--- code/js/controllers/CastboxController.js
+++ code/js/controllers/CastboxController.js
@@ -1,21 +1,20 @@
-;
-(function () {
-  "use strict";
+;(function() {
+    "use strict";
 
-  var BaseController = require("BaseController");
+    var BaseController = require("BaseController");
 
-  new BaseController({
-    siteName: "Castbox",
-    playPause: ".playBtn",
-    playNext: ".forward",
-    playPrev: ".back",
-    mute: ".volumeCtrl",
+    new BaseController({
+        siteName: "Castbox",
+        playPause: ".playBtn",
+        playNext: ".forward",
+        playPrev: ".back",
+        mute: ".volumeCtrl",
 
-    playState: ".playBtn.icon.play",
-    song: ".footerFeed .rightText .title.ellipsis",
-    currentTime: ".currentTime",
-    totalTime: ".duration",
+        playState: ".playBtn.icon.play",
+        song: ".footerFeed .rightText .title.ellipsis",
+        currentTime: ".currentTime",
+        totalTime: ".duration",
 
-    artist: ".footerFeed .rightText .author.ellipsis"
-  });
+        artist: ".footerFeed .rightText .author.ellipsis"
+    });
 })();
