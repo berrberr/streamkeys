a2fd8a5d988a526c22134ba46f0e50d43e82353c
Eric Nemchik <eric@nemchik.com>
2019-09-01 00:09:20 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -45,6 +45,12 @@ module.exports = function(config) {
     browserify: {
       debug: true,
       transform: []
+    },
+
+    client: {
+      jasmine: {
+        random: false
+      }
     }
   });
 };
