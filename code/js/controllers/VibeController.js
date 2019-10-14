49b6b3e75b2bfa1d5c16b4cbe5a356f95b9dbfa1
Junghyeon Park <j824h@outlook.com>
2019-10-14 13:33:59 +0900

    Fixed playState

--- code/js/controllers/VibeController.js
+++ code/js/controllers/VibeController.js
@@ -10,7 +10,7 @@
     playPrev: ".player_controller .control_section .control_area .btn_play_prev",
     mute: ".player_controller .volume_area .btn_volume",
 
-    playState: ".player_controller .control_section .control_area .btn_now play",
+    playState: ".player_controller .control_section .control_area .btn_now.play",
     song: ".song .link a",
     artist: ".artist .link_artist a",
     art: ".thumb_cover img"
