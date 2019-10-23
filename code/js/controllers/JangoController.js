6e36be9c570a567e56f4425023c872af39e06c03
twm <theworldlymonkey@gmail.com>
2019-10-23 14:00:25 +0900

    Added confirmLike and confirmDislike selectors in JangoController

--- code/js/controllers/JangoController.js
+++ code/js/controllers/JangoController.js
@@ -8,7 +8,9 @@
     playPause: "#player_play",
     playNext: "#player_skip",
     like: "#player_thumbs_up",
+    confirmLike: "#thumbs_updown_form_submit > input",
     dislike: "#player_thumbs_down",
+    confirmDislike: "#thumbs_updown_form_submit > input",
 
     playState: "#player_play.player_pause",
     song: "#song_info > span",
