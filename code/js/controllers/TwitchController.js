e3891839cb01863d787eea267d8ae351f908971f
Lukas <HLK65@users.noreply.github.com>
2019-11-10 17:40:17 +0100

    TwitchController fix for redesign

--- code/js/controllers/TwitchController.js
+++ code/js/controllers/TwitchController.js
@@ -5,12 +5,12 @@
 
   var controller = new BaseController({
     siteName: "Twitch.tv",
-    playPause: ".qa-pause-play-button",
-    song: ".title .real"
+    playPause: "[data-a-target='player-play-pause-button']",
+    song: "[data-a-target='stream-title']"
   });
 
   controller.isPlaying = function() {
-    return !!(this.doc().querySelector("span[data-tip='Pause']"));
+    return this.doc().querySelector("[data-a-target='player-play-pause-button']").getAttribute("data-a-player-state") === "playing";
   };
 
 })();
