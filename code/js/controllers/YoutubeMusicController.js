3435170bd4c99e0bf4d016cc6a4f8b3c3c92f14d
Alex Tsitsimpis <alex.tsitsimpis@gmail.com>
2019-11-19 15:39:05 +0200

    YoutubeMusic: Support getting current and total time
    Reviewed-by: Alex Tsitsimpis <alex.tsitsimpis@gmail.com>

--- code/js/controllers/YoutubeMusicController.js
+++ code/js/controllers/YoutubeMusicController.js
@@ -17,23 +17,36 @@
     artist: "artist",
     album: "album",
     art: ".ytmusic-player-bar > .image",
+    currentTime: "currentTime",
+    totalTime: "totalTime",
   });
 
   controller.getSongData = function(selector) {
+    var data = null;
     if (selector === "song") {
       var titleEl = this.doc().querySelector(".content-info-wrapper > .title");
       if (titleEl && titleEl.textContent) {
         return titleEl.textContent;
       }
-    } else {
+
+    } else if ((selector === "currentTime") || (selector === "totalTime")) {
+      var timeInfo = this.doc().querySelector(".ytmusic-player-bar > .time-info");
+      if (timeInfo && timeInfo.textContent) {
+          data = timeInfo.textContent.split("/");
+          if (!data) return null;
+          if (selector === "currentTime" && data.length >= 1) return data[0].trim();
+          if (selector === "totalTime" && data.length >= 2) return data[1].trim();
+      }
+
+    } else if ((selector === "artist") || (selector === "album")) {
       var subTitleEl = this.doc().querySelector(".content-info-wrapper .subtitle > .byline");
       if (subTitleEl && subTitleEl.innerText) {
-        var data = subTitleEl.innerText.split(" • ");
+        data = subTitleEl.innerText.split("•");
         if (!data) return null;
-        if (selector === "artist" && data.length >= 1) return data[0];
-        if (selector === "album" && data.length >= 2) return data[1];
+        if (selector === "artist" && data.length >= 1) return data[0].trim();
+        if (selector === "album" && data.length >= 2) return data[1].trim();
       }
     }
-    return null;
+    return BaseController.prototype.getSongData.call(this, selector);
   };
 })();
