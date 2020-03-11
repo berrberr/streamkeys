17373ee143f43337f92fc97eaa10184e30dd7fd1
Kevin <EmperorEarth@users.noreply.github.com>
2020-03-11 09:09:14 -0400

    bugfix/typo: fast forward/rewind wasn't working

--- code/js/controllers/GooglePodcastsController.js
+++ code/js/controllers/GooglePodcastsController.js
@@ -7,8 +7,8 @@
     siteName: "Google Podcasts",
     play: "div[aria-label=Play]",
     pause: "div[aria-label=Pause]",
-    playNext: "div[aria-label=Fast forward 30 seconds]",
-    playPrev: "div[aria-label=Rewind 10 seconds]",
+    playNext: "div[aria-label='Fast forward 30 seconds']",
+    playPrev: "div[aria-label='Rewind 10 seconds']",
 
     playState: "div[aria-label=Pause]"
   });
