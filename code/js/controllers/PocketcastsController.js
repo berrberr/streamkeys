45a2fc456967b815f97b3ceeb51380ca0c9f5004
Tim Schmidt <tim.schmidt@dowjones.com>
2020-04-25 11:10:06 -0500

    Fix prev button for PocketCasts

--- code/js/controllers/PocketcastsController.js
+++ code/js/controllers/PocketcastsController.js
@@ -6,8 +6,8 @@
   new BaseController({
     siteName: "Pocketcasts",
     playPause: ".play_pause_button",
-    playNext: ".skip_forward_button",
-    playPrev: ".skip_back_button",
+    playNext: ".skip-forward-button",
+    playPrev: ".skip-back-button",
     mute: ".audio_volume_slider",
 
     playState: ".play_pause_button.pause_button",
