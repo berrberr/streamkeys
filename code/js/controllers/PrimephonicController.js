a7b2ceb7cdffe0e6d2c74a369f3c7428700645cc
Wyko Rijnsburger <utwyko@gmail.com>
2020-05-10 09:26:21 +0200

    Add support for Primephonic.

---
+++ code/js/controllers/PrimephonicController.js
@@ -0,0 +1,17 @@
+; (function () {
+    "use strict";
+
+    var BaseController = require("BaseController");
+
+    new BaseController({
+        siteName: "Primephonic",
+        playPause: "#player-play-pause",
+        playNext: "#player-next",
+        playPrev: "#player-prev",
+        mute: "#toggle-volume",
+
+        playState: "#player-play-pause > img[title='Pause']",
+        song: "#currently-playing-work",
+        artist: "#currently-playing-composer",
+    });
+})();
