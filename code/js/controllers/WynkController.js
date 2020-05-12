c80a71ca43ddd2eff83b639649ec131907f7627a
Ashok kumar <ashokkumar01cbe@gmail.com>
2020-05-12 20:03:28 +0530

    added support for Wynk music site
    added support for Wynk music site

---
+++ code/js/controllers/WynkController.js
@@ -0,0 +1,15 @@
+;(function() {
+  "use strict";
+
+  var BaseController = require("BaseController");
+
+  new BaseController({
+    siteName: "Wynk",
+    playPause: ".splay i",
+    playNext: ".snext i",
+    playPrev: ".sprevious i",
+    mute: ".volume i",
+
+    playState: "[title='Pause']"
+  });
+})();
