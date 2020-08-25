07af3a07cb5d117d29c5e99dcc2133ea0ce99899
Jantzen Ward <jward@degdigital.com>
2020-08-25 18:00:47 -0500

    Add ability to dislike/block tracks in Tidal

--- code/js/controllers/TidalController.js
+++ code/js/controllers/TidalController.js
@@ -2,15 +2,25 @@
 (function() {
   var BaseController = require("BaseController");
 
-  new BaseController({
+  var controller = new BaseController({
     siteName: "Tidal",
     play: "[data-test=play]",
     pause: "[data-test=pause]",
     playNext: "[data-test=next]",
     playPrev: "[data-test=previous]",
+    block: "[data-test=block]",
+    blockTrack: "[data-test=block-track]",
+    dislike: "#override",
 
     song: "[data-test=track-title]",
     artist: "[data-test=artist-title]",
     buttonSwitch : true
   });
+
+  controller.dislike = function() {
+    if(!document.querySelector(this.selectors.blockTrack)) {
+      document.querySelector(this.selectors.block).click();
+    }
+    document.querySelector(this.selectors.blockTrack).click();
+  };
 })();
