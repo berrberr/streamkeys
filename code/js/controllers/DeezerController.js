0340d8a6c759614ee8238bcf064f60db5519c1d8
Jantzen Ward <jward@degdigital.com>
2020-09-12 22:31:32 -0500

    Add support for disliking songs on Deezer

--- code/js/controllers/DeezerController.js
+++ code/js/controllers/DeezerController.js
@@ -2,17 +2,25 @@
 (function() {
   var BaseController = require("BaseController");
 
-  new BaseController({
+  var controller = new BaseController({
     siteName: "Deezer",
 
     playPrev: "div.player-controls > ul > li:nth-child(1) > div > button",
     playPause: "div.player-controls > ul > li:nth-child(3) > button",
     playNext: "div.player-controls > ul > li:nth-child(5) > div > button",
     playState: "svg.svg-icon-pause",
+    dislike: "svg.svg-icon-angry-face",
 
     mute: "div.player-options > ul > li:nth-child(1) > ul > li:nth-child(3) > div > button",
 
     song: "a.track-link:nth-of-type(1)",
     artist: "a.track-link:nth-of-type(2)"
   });
+
+  controller.dislike = function() {
+    if(!document.querySelector(".svg-icon-note-block")) {
+      document.querySelector(".svg-icon-angry-face").closest("button").click();
+    }
+    document.querySelector(".svg-icon-note-block").closest("div").click();
+  };
 })();
