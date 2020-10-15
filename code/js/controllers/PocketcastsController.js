6adcc318c360ee3bbf0fc943cf9f4b43cd3f7293
Tim Schmidt <tim.schmidt@dowjones.com>
2020-10-15 12:04:39 -0500

    Fixing Pocketcasts play button

--- code/js/controllers/PocketcastsController.js
+++ code/js/controllers/PocketcastsController.js
@@ -4,12 +4,12 @@
 
   new BaseController({
     siteName: "Pocket Casts",
-    playPause: ".play_pause_button",
+    playPause: "button.skip_back_button + button",
     playNext: ".skip-forward-button",
     playPrev: ".skip-back-button",
     mute: ".player-controls-mute-button",
 
-    playState: ".play_pause_button.pause_button",
+    playState: "button.skip_back_button + button[aria-pressed=true]",
     song: ".player_episode",
     artist: ".player_podcast_title"
   });
