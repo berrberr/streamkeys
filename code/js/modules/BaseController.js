7fa66afa57d1022d5afd09c4f49fae80aba4941f
The Worldly Monkey <56583786+the-worldly-monkey@users.noreply.github.com>
2020-12-04 04:44:43 +0900

    Updated JangoController, added automatic confirmation of like and dislike (for Jango) (#623)
    * Update to 1.8.0. Change prod build to not uglify per chrome extension
    updated standards & fix manifest warnings.
    
    * Fix Jango controller to reference new elements
    
    * Fix Jango controller to reference new elements (#538)
    
    * New feature: added selectors for automatic confirmation of like/dislike actions (to be used with websites that require it, e.g. Jango)
    
    * Added confirmLike and confirmDislike selectors in JangoController
    
    * Disconnection of the MutationObserver is now the first instruction of the callback (cleaner)
    
    * Updated Jango controller (UI renewed, 2020/06/22)
    
    * JangoController: fixed wrong selector for artist
    
    Co-authored-by: Alex Gabriel <alex.gabriel@instacart.com>

--- code/js/modules/BaseController.js
+++ code/js/modules/BaseController.js
@@ -14,7 +14,9 @@
       playPrev: (options.playPrev || null),
       mute: (options.mute || null),
       like: (options.like || null),
+      confirmLike: (options.confirmLike || null),
       dislike: (options.dislike || null),
+      confirmDislike: (options.confirmDislike || null),
       iframe: (options.iframe || null),
 
       //** States **//
@@ -144,10 +146,26 @@
 
   BaseController.prototype.like = function() {
     this.click({action: "like", selectorButton: this.selectors.like, selectorFrame: this.selectors.iframe});
+    if(this.selectors.confirmLike !== null) {
+      var controller = this;
+      var observer = new MutationObserver(function() {
+        this.disconnect();
+        controller.click({action: "confirmLike", selectorButton: controller.selectors.confirmLike, selectorFrame: controller.selectors.iframe});
+      });
+      observer.observe(document.documentElement, {childList: true, subtree: true});
+    }
   };
 
   BaseController.prototype.dislike = function() {
     this.click({action: "dislike", selectorButton: this.selectors.dislike, selectorFrame: this.selectors.iframe});
+    if(this.selectors.confirmDislike !== null) {
+      var controller = this;
+      var observer = new MutationObserver(function() {
+        this.disconnect();
+        controller.click({action: "confirmDislike", selectorButton: controller.selectors.confirmDislike, selectorFrame: controller.selectors.iframe});
+      });
+      observer.observe(document.documentElement, {childList: true, subtree: true});
+    }
   };
 
   /**
