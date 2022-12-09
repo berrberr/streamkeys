6565649a2a06a78ceafeabb64e1c97e8e0e90654
Conrado Gouvea <conradoplg@gmail.com>
2022-12-09 00:06:59 -0300

    update Apple Music support

--- code/js/controllers/AppleMusicController.js
+++ code/js/controllers/AppleMusicController.js
@@ -1,17 +1,50 @@
 "use strict";
-(function() {
+(function () {
   var BaseController = require("BaseController");
 
-  new BaseController({
+  var controller = new BaseController({
     siteName: "Apple Music",
-    play: "div.web-chrome-playback-controls__main > button:nth-child(2)",
-    pause: "div.web-chrome-playback-controls__main > button:nth-child(2)",
-    playNext: "div.web-chrome-playback-controls__main > button:nth-child(3)",
-    playPrev:
-      "div.web-chrome-playback-controls__main > button.button-reset.web-chrome-playback-controls__playback-btn.web-chrome-playback-controls__playback-btn--previous",
 
-    playState: "body > div.web-navigation > div > div.web-chrome.is-playing",
-    song: "#playback-sub-copy > div > div > span:nth-child(1) > span",
-    buttonSwitch: true
+    // "fake" selectors used in getSongData()
+    song: "song",
+    artist: "artist",
+    album: "album",
+
+    buttonSwitch: true,
+    overridePlayPause: true,
+    overridePlayNext: true,
+    overridePlayPrev: true
   });
+
+  controller.isPlaying = function () {
+    return !document.querySelector("amp-chrome-player")?.shadowRoot.querySelector("apple-music-playback-controls")?.shadowRoot.querySelector(".playback-play__pause")?.ariaHidden;
+  };
+
+  controller.playPause = function () {
+    if (this.isPlaying()) {
+      document.querySelector("amp-chrome-player")?.shadowRoot.querySelector("apple-music-playback-controls")?.shadowRoot.querySelector(".playback-play__pause")?.click();
+    } else {
+      document.querySelector("amp-chrome-player")?.shadowRoot.querySelector("apple-music-playback-controls")?.shadowRoot.querySelector(".playback-play__play")?.click();
+    }
+  };
+
+  controller.playPrev = function () {
+    document.querySelector("amp-chrome-player")?.shadowRoot.querySelector("apple-music-playback-controls")?.shadowRoot.querySelector(".previous")?.click()
+  }
+
+  controller.playNext = function () {
+    document.querySelector("amp-chrome-player")?.shadowRoot.querySelector("apple-music-playback-controls")?.shadowRoot.querySelector(".next")?.click()
+  }
+
+  controller.getSongData = function (selector) {
+    var r = null;
+    if (selector == "song") {
+      r = document.querySelector("amp-lcd")?.shadowRoot.querySelector(".lcd-meta__primary-wrapper .lcd-meta-line__fragment")?.textContent;
+    } else if (selector == "artist") {
+      r = document.querySelector("amp-lcd")?.shadowRoot.querySelector(".lcd-meta__secondary .lcd-meta-line__fragment:first-child")?.textContent;
+    } else if (selector == "album") {
+      r = document.querySelector("amp-lcd")?.shadowRoot.querySelector(".lcd-meta__secondary .lcd-meta-line__fragment:last-child")?.textContent;
+    }
+    return r ?? null;
+  };
 })();
